38f5a5f3d01f9241bdf23897ad8914e4058352ec
mierdev <smurfmier@gmail.com>
2024-04-13 00:16:43 +0200

    started om letter counting function

--- main.py
+++ main.py
@@ -2,16 +2,28 @@ def main():
   book_title = "Frankenstein"
   book_file = "books/frankenstein.txt"
   words = get_book_text(book_file)
-  count = word_count(words)
-  print(f"{book_title} has {count} words!")
+  amount_of_words = count_words(words)
+  amount_of_letters = count_letters(words)
+
+  print(f"{book_title} has {amount_of_words} words!")
+  print(amount_of_letters)
 
 def get_book_text(book):
   with open(book) as text:
     return text.read()
 
-def word_count(book):
+def count_words(book):
   words = book.split()
   return len(words)
+
+def count_letters(words):
+  letters_dictionary = {}
+  for word in words:
+    lowercase = word.lower().split()
+    
+    # AARRRGGGHHH
+
+  return letters_dictionary
 
 
 
