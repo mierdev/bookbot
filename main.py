810c0935f973bdb99eb24a36d0476f4596d5990f
mierdev <smurfmier@gmail.com>
2024-04-13 12:03:12 +0200

    converted dict characters to list with dict, and accessed them

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ def main():
   book_words = get_book_text(book_file)
   amount_of_words = count_words(book_words)
   amount_of_letters = count_letters(book_words)
+  list_of_characters = convert_to_list_of_dictionaries(amount_of_letters)
 
-  report(book_file, book_title, amount_of_words, amount_of_letters)
+  report(book_file, book_title, amount_of_words, list_of_characters)
 
 def get_book_text(book):
   with open(book) as text:
@@ -28,6 +29,14 @@ def count_letters(words):
 
   return character_dictionary
 
+def convert_to_list_of_dictionaries(characters_count):
+  list_of_characters = []
+  for each in characters_count:
+    list_of_characters.append({each: characters_count[each]})
+  
+  return list_of_characters
+
+
 def report(book_path, book_title, words_count, characters_count):
   print(f"--- Begin report of {book_path} ---")
   
@@ -38,9 +47,9 @@ def report(book_path, book_title, words_count, characters_count):
   
   print("\n\n")
 
-  
-  for each in characters_count:
-    print(f"The '{each}' character was found {characters_count[each]} times")
+  for element in characters_count:
+   for key, value in element.items():
+      print(f"The '{key}' character was found {value} times")
 
   print("\n\n")
 
